ee873a249c30952a860aedfa7ebd83c5a281aa92
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2020-11-10 12:00:57 +0800

    all finish

--- cradtest/Assets/Scripts/GameManager.cs
+++ cradtest/Assets/Scripts/GameManager.cs
@@ -14,6 +14,11 @@ public class GameManager : MonoBehaviour
     public float turnspeed = 1.5f;
     [Header("縮放倍率"), Range(0.1f, 5f)]
     public float scalesize = 2.5f;
+    [Header("動畫")]
+    public Animator anibehold;
+    public Animator anichest;
+
+    public float x = 0;
 
 
 
@@ -26,6 +31,20 @@ public class GameManager : MonoBehaviour
         print(Joystick.Vertical);
         behold.Rotate(0, Joystick.Horizontal, 0);
         chest.Rotate(0, Joystick.Horizontal * turnspeed, 0);
-        behold.transform.localScale = new Vector3(1, 1, 1) * Joystick.Vertical * scalesize;
+
+     
+            x = Mathf.Clamp(Joystick.Vertical, 0.2f, 1);
+
+
+            behold.transform.localScale = new Vector3(1, 1, 1) * x * scalesize;
+            chest.transform.localScale = new Vector3(1, 1, 1) * x * scalesize;
+        
+        
+    }
+
+    public void anicontroll(string anicall)
+    {
+        anibehold.SetTrigger(anicall);
+        anichest.SetTrigger(anicall);
     }
 }
